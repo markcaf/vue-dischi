6558b818d9614062e61a48715ae366b90e860904
markcaf <marcocafassog@gmail.com>
2022-07-20 01:33:13 +0200

    Created the imported Main component's markup

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,191 @@
 <template>
-  <div>
-  </div>
+  <main>
+    <div class="container py-5">
+        <div class="row d-flex flex-wrap gap-4 justify-content-center">
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+
+            <div class="col-2 album-card text-center">
+                <div class="image-container">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg" alt="album cover">
+                </div>
+
+                <div class="title">
+                    One more car, one more raider
+                </div>
+                
+                <div class="author">
+                    Eric Clapton
+                </div>
+
+                <div class="year">
+                    2022
+                </div>
+            </div>
+        </div>
+    </div>
+
+  </main>
 </template>
 
 <script>
@@ -9,6 +194,42 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    main{
+        background-color: #1e2d3b;
 
+        .container{
+            max-width: 1000px;
+
+            .album-card{
+                background-color: #2e3a46;
+                height: 320px;
+
+                .image-container{
+                    padding: 10px;
+                    margin-top: 10px;
+
+                    img{
+                        width: 100%;
+                    }
+                }
+
+                .title{
+                    text-transform: uppercase;
+                    color: white;
+                    font-weight: bold;
+                    padding-top: 10px;
+                    margin-bottom: 20px;
+                    font-size: 20px;
+                    line-height: 22px;
+                }
+
+                .author,
+                .year{
+                    color: #6c7675;
+                }
+
+            }
+        }
+    }
 </style>
