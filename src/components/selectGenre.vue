14466ef827c22a54da4d41903ba4838ff093c27b
markcaf <marcocafassog@gmail.com>
2022-07-21 01:11:06 +0200

    Created the component of the select w/ props

---
+++ src/components/selectGenre.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-4 mb-4">
+    <select class="form-select" aria-label="Select by genre">
+        <option selected>Filtra in base al genere</option>
+        <option class="text-uppercase" v-for="(content, index) in contents" :key="index" :value="index">
+          {{ content }}
+        </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "selectGenre",
+
+    props: {
+        data: String,
+        contents: Array,
+    },
+
+    data: function() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
